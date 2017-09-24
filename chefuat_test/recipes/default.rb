8919f9dd3ee03b1e8fe1d92a0d165012f38b6a54
ganeshgiri1984 <ganeshgiri.1984@gmail.com>
2017-09-24 10:08:38 -0700

    First commit 1.0

---
+++ chefuat_test/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: chefuat_test
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+directory '/opt/ganesh1234' do
+	action :create
+end
